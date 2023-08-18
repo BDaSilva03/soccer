8b65a633a18ebc6ee7f2531c42191b8887cd473e
Brandon DaSilva <bd282@njit.edu>
2023-08-18 13:55:47 -0400

    finished bare-bone frontend

--- frontend/soccer/src/components/GuessPlayer.js
+++ frontend/soccer/src/components/GuessPlayer.js
@@ -1,33 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import PlayerInfo from './PlayerInfo';
+import { normalizeText } from './NormalizeText';
 import axios from 'axios';
 
 function GuessPlayer() {
   const [player, setPlayer] = useState(null);
   const [guess, setGuess] = useState('');
   const [correct, setCorrect] = useState(false);
+  const [correctGuesses, setCorrectGuesses] = useState(0); // 1. Correct guesses counter
+  const [message, setMessage] = useState('');
+  const [messageColor, setMessageColor] = useState('');
+  const [showPhoto, setShowPhoto] = useState(false);
   
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/randomPlayer');
-        setPlayer(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
+    fetchNewPlayer();
   }, []);
 
+  const fetchNewPlayer = async () => {
+    try {
+      const response = await axios.get('/randomPlayer');
+      setPlayer(response.data);
+      setGuess('');
+      setCorrect(false);
+      setMessage('');
+      setShowPhoto(false);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   const handleGuess = () => {
-    if (player && guess.toLowerCase() === player.name.toLowerCase()) {
-      setCorrect(true);
+    if (player) {
+      const normalizedGuess = normalizeText(guess);
+      const lastNames = player.lastname.split(' ').map(name => normalizeText(name));
+      const fullName = normalizeText(player.name);
+
+      if (normalizedGuess === fullName || lastNames.includes(normalizedGuess)) {
+        setCorrect(true);
+        setMessage('Correct Guess!');
+        setMessageColor('green');
+        setCorrectGuesses(prev => prev + 1); // Increment correct guesses
+      } else {
+        setMessage('Incorrect! Try again.');
+        setMessageColor('red');
+        setShowPhoto(true);
+        setCorrectGuesses(0); // Reset correct guesses counter
+      }
     }
   };
 
   return (
     <div>
+      <div className="correct-guesses">Correct guesses in a row: {correctGuesses}</div>
       {player ? (
         <div>
           <PlayerInfo player={player} />
@@ -37,8 +62,10 @@ function GuessPlayer() {
             onChange={(e) => setGuess(e.target.value)}
             placeholder="Guess the player's name"
           />
-          <button onClick={handleGuess}>Submit</button>
-          {correct && <p>Correct Guess!</p>}
+          {!correct && <button onClick={handleGuess}>Submit</button>}
+          {correct && <button onClick={fetchNewPlayer}>Start New Game</button>}
+          <p style={{ color: messageColor }}>{message}</p>
+          {showPhoto && !correct && <img src={player.photo} alt="Player" />}
         </div>
       ) : (
         <p>Loading...</p>
--- frontend/soccer/src/components/PlayerInfo.js
+++ frontend/soccer/src/components/PlayerInfo.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { normalizedCountryToCode } from './countryCodes';
+import { normalizeText } from './NormalizeText';
 
 function PlayerInfo({ player }) {
+  const countryCode = normalizedCountryToCode[normalizeText(player.nationality)] || 'unknown';
+  
+  // Determine the flag URL based on the country
+  let flagURL;
+  if (['gb-eng', 'ie', 'gb-sct', 'gb-wls', 'gb-nir'].includes(countryCode.toLowerCase())) {
+    flagURL = `https://flagcdn.com/64x48/${countryCode.toLowerCase()}.png`;
+  } else {
+    flagURL = `https://flagsapi.com/${countryCode.toUpperCase()}/flat/64.png`;
+  }
+
   return (
     <div>
       <p>Age: {player.age}</p>
-      <p>Nationality: {player.nationality}</p>
-      <p>Team: {player.team_name}</p>
+      <img src={flagURL} alt={player.nationality} title={player.nationality} />
+      <img src={player.team_logo} alt={player.team_name} title={player.team_name} />
     </div>
   );
 }
