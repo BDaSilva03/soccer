44d3f3c89846a3446b172bdd110a467c8fc41637
Brandon DaSilva <bd282@njit.edu>
2023-08-18 10:14:36 -0400

    GuessPlayer component

---
+++ frontend/soccer/src/components/GuessPlayer.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import PlayerInfo from './PlayerInfo';
+import axios from 'axios';
+
+function GuessPlayer() {
+  const [player, setPlayer] = useState(null);
+  const [guess, setGuess] = useState('');
+  const [correct, setCorrect] = useState(false);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_BACKEND_URL + '/randomPlayer');
+        setPlayer(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleGuess = () => {
+    if (player && guess.toLowerCase() === player.name.toLowerCase()) {
+      setCorrect(true);
+    }
+  };
+
+  return (
+    <div>
+      {player ? (
+        <div>
+          <PlayerInfo player={player} />
+          <input 
+            type="text" 
+            value={guess} 
+            onChange={(e) => setGuess(e.target.value)}
+            placeholder="Guess the player's name"
+          />
+          <button onClick={handleGuess}>Submit</button>
+          {correct && <p>Correct Guess!</p>}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default GuessPlayer;
