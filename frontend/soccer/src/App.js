678b19a2edc1ba9ae2cf69258948aeab90aa332c
Brandon DaSilva <bd282@njit.edu>
2023-08-18 16:26:47 -0400

    included register and login on app

--- frontend/soccer/src/App.js
+++ frontend/soccer/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import GuessPlayer from './components/GuessPlayer';
+import Register from './components/Register'; // Import Register
+import Login from './components/Login'; // Import Login
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Guess the Soccer Player</h1>
-      <GuessPlayer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Guess the Soccer Player</h1>
+            <Register />
+            <Login />
+            <GuessPlayer />
+        </div>
+    );
 }
 
 export default App;
